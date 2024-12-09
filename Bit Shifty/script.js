2c2bc6b8600ca3443bcfbdb1913e0192bc29ae70
KASEYCLAY <kasey.louis-clayton@jdplc.com>
2024-12-09 13:39:57 +0000

    commit

--- Bit Shifty/script.js
+++ Bit Shifty/script.js
@@ -2,13 +2,14 @@ const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
 let lastChecked;
 
-function handcleCheck(e) {
+function handleCheck(e) {
     let inBetween = false;
-if(e.shiftKey && this.checked) {
- checkboxes.forEach(checkbox => {
- console.log(checkbox);  
- });
-}
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+        });
+    }
     lastChecked = this;
 }
-checkboxes.forEach(checkbox => checkbox.addEventListener('change', handleCheck))
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('change', handleCheck));
